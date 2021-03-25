7bd8bdcff4e85e0a985574a06a27b28d45be9a2e
niramay447 <54571224+niramay447@users.noreply.github.com>
2021-03-25 17:36:29 +0530

    authenticating the sheet api

--- main.py
+++ main.py
@@ -45,6 +45,28 @@ for exercise in result["exercises"]:
         }
     }
 
+    # No Authentication
     sheet_response = requests.post(sheet_endpoint, json=sheet_inputs)
+
+    # Basic Authentication
+    sheet_response = requests.post(
+        sheet_endpoint,
+        json=sheet_inputs,
+        auth=(
+            "YOUR USERNAME",
+            "YOUR PASSWORD",
+        )
+    )
+
+    # Bearer Token Authentication
+    bearer_headers = {
+        "Authorization": "Bearer YOUR_TOKEN"
+    }
+    sheet_response = requests.post(
+        sheet_endpoint,
+        json=sheet_inputs,
+        headers=bearer_headers
+    )
+
 
     print(sheet_response.text)
